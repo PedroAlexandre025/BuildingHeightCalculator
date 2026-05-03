a59afcf6b4a277677c1bfad32d546228a1e5d940
clebaoxd <pedro612007@gmail.com>
2026-05-03 03:43:41 -0300

    Versão 1

--- src/classes/InputsValidation.java
+++ src/classes/InputsValidation.java
@@ -1,40 +1,41 @@
 package classes;
 
 public class InputsValidation {
+	//teto de velocidade plausível para o modelo
+	//usado para restrição, pois o usuario pode digitar uma altura de por exemplo 10 m para janela
+	// e 0.01 s para tempo, dando uma velociade impossivel de 10/0.01 = 1000m/s, algo supersonico.
+	private static final double MAX_SPEED = 100.0;
 
-    private double windowHeight;
-    private double timeInWindow;
-    private double fallAndBackTime;
+	public static double getMaxSpeed(){
+		return MAX_SPEED;
+	}
 
-    public InputsValidation(double fallAndBackTime, double timeInWindow, double windowHeight) {
-        this.fallAndBackTime = fallAndBackTime;
-        this.timeInWindow = timeInWindow;
-        this.windowHeight = windowHeight;
-    }
+	public int validation(double windowHeight, double timeInWindow, double fallAndBackTime ) {
 
-    public int validation() {
+		//tamanho da janela não pode ser negativo e maior que 17.2(janela mais alta construida no mundo)
+		if (windowHeight <= 0 ) {
+			return 1;
+		}
 
-        if (windowHeight <= 0) {
-            return 1;
-        }
+		// o tempo na janela não pode ser maior ou igual a raiz quadrada de 2*altura da
+		// janela/gravidade,
+		// pois na equação de dy = v0*t + (1/2)*at², quando isolamos o v0, t passa
+		// dividindo, logo ele necessariamente é >0
+		// então para equação ser valida (positiva) só depende do numerador. logo:
+		// dy - 1/2gt² > 0
+		// dy > 1/2gt²
+		// 2dy>gt²
+		// 2dy/g>t²
+		// t < sqrt(2dy/g)
+		//o tempo também não pode ser menor ou igual a windowHeight/100.0
+		if (timeInWindow<= windowHeight/MAX_SPEED  || timeInWindow >= Math.sqrt((2 * windowHeight) / 9.8)) {
+			return 2;
+		}
 
+		if (fallAndBackTime <= 0 || fallAndBackTime <= 2 * timeInWindow) {
+			return 3;
+		}
 
-        //o tempo na janela não pode ser maior ou igual a raiz quadrada de 2*altura da janela/gravidade,
-        //pois na equação de dy = v0*t + (1/2)*at², quando isolamos o v0, t passa dividindo, logo ele necessariamente é >0
-        //então para equação ser valida (positiva) só depende do numerador. logo:
-        //dy - 1/2gt² > 0
-        //dy > 1/2gt²
-        //2dy>gt²
-        //2dy/g>t²
-        //t < sqrt(2dy/g)
-        if (timeInWindow <= 0 || timeInWindow >= Math.sqrt((2 * windowHeight) / 9.8)) {
-            return 2;
-        }
-
-        if (fallAndBackTime <= 0 || fallAndBackTime <= 2 * timeInWindow) {
-            return 3;
-        }
-
-        return 0;
-    }
+		return 0;
+	}
 }
--- src/classes/Interface.java
+++ src/classes/Interface.java
@@ -6,83 +6,284 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class Interface extends JFrame implements ActionListener {
 
-    public Interface() {
+
+	private int valid;
+	private double height, fallT, windowT, heightBuilding;
+	private JTextField heightField, windowTime, fallBack;
+	private JLabel result;
+	private JButton calc, reset, simulateButton;
+	private Simulation sim;
+	private Calculator calculator;
+
+
+	public Interface() {
+
+		setSize(400, 720);// tam janela
+		setTitle("Calculadora");//titulo
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //encerra o programa quando fecha janela
+		setLocationRelativeTo(null); //abrir janela no meio da tela
+		setLayout(null);
+
+		JLabel WindowHeight = new JLabel("Altura da Janela (m): "); //texto para pedir altura
+		WindowHeight.setBounds(65, 30, 130, 30);
+		heightField = new JTextField(); 	//caixa de input de altura da janela
+		heightField.setBounds(225, 30, 100, 30);
+
+		JLabel timeInWindow = new JLabel("Tempo na janela (s): ");//texto para pedir tempo de queda na janela
+		timeInWindow.setBounds(65, 65, 130, 30);
+		windowTime = new JTextField(); 							//input
+		windowTime.setBounds(225, 65, 100, 30);
+
+		JLabel fallBackTime = new JLabel("Queda e volta(s): ");//texto para pedir tempo de queda e volta da bola
+		fallBackTime.setBounds(65, 100, 130, 30);
+		fallBack = new JTextField();							//input
+		fallBack.setBounds(225, 100, 100, 30);
+
+		reset = new JButton("Resetar"); //botao reset
+		calc = new JButton("Calcular"); //botao calcular
+		simulateButton = new JButton("Ver Simulação");
+
+
+		calc.setBounds(65, 140, 130, 30);
+		reset.setBounds(225, 140, 100, 30);
+		simulateButton.setBounds(140, 250, 120, 30);
+
+		calc.setForeground(new Color(0, 0, 0));
+		reset.setForeground(new Color(165, 12, 12));
 
 
 
-        setSize(400, 600);
-        setTitle("Calculadora");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(null);
+		JPanel output = new JPanel(); //painel de output
+		output.setBounds(65, 180, 260, 60);
+		output.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY), "Altura do prédio",
+				TitledBorder.LEFT, TitledBorder.TOP)); //setar cores e borda do output
+		output.setLayout(new GridBagLayout());
+		result = new JLabel(); //texto resultado
+		result.setText("0,00m");
+		result.setBounds(150, 200, 50, 40);
+		result.setForeground(new Color(10, 90, 170));
+		result.setFont(new Font("Arial", Font.BOLD, 20));
 
-        JLabel WindowHeight = new JLabel("Altura da Janela (m): ");
-        WindowHeight.setBounds(65, 30, 130, 30);
-        JTextField heightField = new JTextField();
-        heightField.setBounds(225, 30, 100,30);
-
-        JLabel timeInWindow = new JLabel("Tempo na janela (s): ");
-        timeInWindow.setBounds(65, 65, 130, 30);
-        JTextField windowTime= new JTextField();
-        windowTime.setBounds(225, 65, 100,30);
-
-        JLabel fallBackTime = new JLabel("Queda e volta(s): ");
-        fallBackTime.setBounds(65, 100, 130, 30);
-        JTextField fallBack= new JTextField();
-        fallBack.setBounds(225, 100, 100,30);
-
-        JButton reset = new JButton("Resetar");
-        JButton calc = new JButton("Calcular");
-
-        calc.setBounds(65, 140, 130, 30);
-        reset.setBounds(225, 140, 100, 30);
-        calc.setForeground(new Color(0, 0, 0));
-        reset.setForeground(new Color(165, 12, 12));
-
-        JPanel output = new JPanel();
-        output.setBounds(65, 180, 260, 60);
-        output.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createLineBorder(Color.LIGHT_GRAY),
-                "Resultado",
-                TitledBorder.LEFT,
-                TitledBorder.TOP
-        ));
-        output.setLayout(new GridBagLayout());
-        JLabel result = new JLabel();
-        result.setText("0,00m");
-        result.setBounds(150, 200, 50, 40);
-        result.setForeground(new Color(10, 90, 170));
-        result.setFont(new Font("Arial", Font.BOLD, 20));
-
-        String h = heightField.getText();
-        String t1 = windowTime.getText();
-        String t2 = fallBack.getText();
-
-        add(WindowHeight);
-        add(heightField);
-
-        add(timeInWindow);
-        add(windowTime);
-
-        add(fallBackTime);
-        add(fallBack);
-        output.add(result);
-        add(output);
+		calc.addActionListener(this); //adicionar metodo de funcionalidade dos botões
+		reset.addActionListener(this);
+		simulateButton.addActionListener(this);
 
 
 
-        add(reset);
-        add(calc);
+		//adicionar cada coisa a interface
+		add(WindowHeight);
+		add(heightField);
 
-        calc.addActionListener(this);
-        setVisible(true);
+		add(timeInWindow);
+		add(windowTime);
 
-    }
+		add(fallBackTime);
+		add(fallBack);
+		output.add(result);
+		add(output);
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        //CONTINUAR EM: validação de entrada: texto vazio ou letras e conversão dos inputs de String para double
-    }
+		add(reset);
+		add(calc);
+
+
+		simulateButton.setVisible(false);
+
+
+		add(simulateButton);
+
+
+
+		//instanciar metodo simulacao
+
+
+		//deixar interface visivel
+		setVisible(true);
+
+	}
+	//metodo de funcionalidade de botões
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		//funcionalidade botão calcular
+		if(e.getSource()==calc) {
+			try{
+				String h = heightField.getText();
+				String t1 = windowTime.getText();
+				String t2 = fallBack.getText();		//conversão das caixas de string para double
+				height = Double.parseDouble(h);
+				windowT = Double.parseDouble(t1);
+				fallT = Double.parseDouble(t2);
+
+				valid = new InputsValidation().validation(height, windowT, fallT); //metódo de classe para validação de dados
+
+				if(valid == 1) {
+					printErrorMessage("Erro 1","Altura inválida da janela! Deve ser maior que zero" ); //valor 1 retorna erro no primeiro input
+
+				}
+				else if(valid == 2) {
+					String err2_0 = String.format("%.2f", height/ InputsValidation.getMaxSpeed());
+					String err2_1 = String.format("%.2f", Math.sqrt((2 * height) / 9.8));
+					printErrorMessage("Erro 2","Tempo na janela Inválido! Deve ser maior que "+err2_0+ " e menor que "+err2_1 ); //valor 2 retorna erro no segundo input
+
+				}else if (valid ==3) {
+					String err3 = String.format("%.2f", 2*windowT);
+					printErrorMessage("Erro 3","Tempo de queda e volta inválido! Deve ser maior que "+err3 ); //valor 3 retorna erro no terceiro input
+
+				}
+				else {
+					calculator = new Calculator(height, windowT, fallT);
+					heightBuilding = calculator.Calculate(); //se passar na verificação calcula
+					String res = String.format("%.2f", heightBuilding);
+					result.setText(res+"m");
+					simulateButton.setVisible(true);
+
+				}
+
+			}catch(NumberFormatException ex) {
+			printErrorMessage("Erro de Entrada", "Insira dados válidos"); //verificação para inputs diferentes de números
+			}
+
+		}
+		else if(e.getSource() == reset) {//função botão reset, limpar as caixas e zerar o resultado e remover simulação da tela
+			heightField.setText(null);
+			fallBack.setText(null);     	//valores nulos nos inputs
+			windowTime.setText(null);
+			result.setText("0,00m");//0 no output
+			simulateButton.setVisible(false);//simulação invisivel
+			sim.timer.stop();//parar o timer dela
+			remove(sim);//remover da tela
+
+			repaint();	//pintar novamente
+			revalidate();
+
+		}
+		else if(e.getSource() == simulateButton){ //botão para gerar quadro de simulação
+			sim = new Simulation();
+			sim.setBounds(0, 300, 400, 400);
+			sim.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY), "",
+					TitledBorder.LEFT, TitledBorder.TOP));
+			sim.setLayout(new GridBagLayout());
+			add(sim);
+			sim.setVisible(true);
+			repaint();
+			revalidate();
+
+
+		}
+
+	}
+
+	//metodo para printar erro
+	public void printErrorMessage(String title, String error) {
+		JOptionPane.showMessageDialog(null,  error, title, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+
+
+
+
+	//classe de simulação
+	//tamanho setado do painel 400x400
+	public class Simulation extends JPanel{
+		private static final double SCALE = 1.2;//escala base em pixels para definir a altura do prédio dinamicamente de acordo com resultado.
+		private static final double windowScale = 3.0; // escala para a janela não ficar muito pequena
+		private int pixelsBuildingHeight, pixelsWindowTop, buildingPosition;
+		private Timer timer;	//timer para simulação
+		private double ballY, windowBasePixels;
+		private int goingUp = 0;	//ints para gerenciamento de maquina de estados
+		private int valid =0;
+
+		public Simulation(){
+			pixelsBuildingHeight = (int)(heightBuilding * SCALE);//calculo da altura em pixels
+
+			double positionTopWindow = 0.5*9.8*(calculator.getTimeRoofToWindowPeak()*calculator.getTimeRoofToWindowPeak()); //calculo da posição do topo da janela usando y - y0 = v0*t + (1/2)*at²
+
+
+			if(pixelsBuildingHeight < 120) {
+				pixelsBuildingHeight = 120;
+			}
+			else if(pixelsBuildingHeight < 210) {	//verificações para o tamanho do prédio em px para nao estourar o tamanho do painel e nem ficar muito pequeno
+				pixelsBuildingHeight = 210;			// definindo base (120), ponto médio (210) e ponto maximo (350)
+			}
+			else if(pixelsBuildingHeight > 350) {
+				pixelsBuildingHeight = 350;
+			}
+
+			buildingPosition = 380-pixelsBuildingHeight;//calculo da posição onde o prédio começa a ser desenhado
+			ballY = buildingPosition;//definir altura onde a bola começa a cair
+			pixelsWindowTop = (int)(positionTopWindow*windowScale)+buildingPosition; //ṕosição do topo da janela em pixels
+			 windowBasePixels= pixelsWindowTop+(calculator.getWindowHeight()*windowScale); //posição da base da janela em pixels
+
+
+			setOpaque(false);
+
+			double frameSpeed =  pixelsBuildingHeight/(calculator.getTotalTime()*60); //calculo da velocidade por frame  altura em pixels/tempo total*frames por segundo
+
+
+
+
+			timer = new Timer(16, e ->{//maquina de estados para simulação
+
+				if (goingUp ==0){
+					ballY += frameSpeed; //bolinha começa a cair
+					if (ballY >= 375){ //condicional para se chegar no chão
+
+						if (valid ==2){
+							goingUp = 2;//se valid = 2 vai para o 3 estado
+						}
+						else if(valid==3){
+							timer.stop();//se valid =3 acaba a simulação
+						}else {
+							goingUp = 1;
+							valid++;	//senao, vai para o segundo e estado e incrementa valid (1)
+						}
+					}
+					repaint();
+				}
+				else if(goingUp == 1){
+					ballY-= frameSpeed;//bola começa a subir
+
+					if (ballY<=windowBasePixels){//quando chegar ao peitoril da janela incrementa valid (2)e volta para o primeiro estado
+						valid++;
+						goingUp = 0;
+					}
+					repaint();
+				}else {//bola subindo novamente, mas apenas metade do caminho
+					ballY-=frameSpeed;
+					if (ballY<=windowBasePixels+(380-windowBasePixels)/2){//condicional para ir até metade do caminho do peitoril da janela
+						goingUp = 0;//volta ao primeiro estado
+						valid++; //valid (3)
+					}
+					repaint();
+				}
+			});
+			timer.start();
+
+		}
+
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			//predio
+			g.setColor(Color.blue);
+			g.fillRect(160, buildingPosition, 80, pixelsBuildingHeight);
+			//janela
+			g.setColor(Color.gray);
+			g.fillRect(185, pixelsWindowTop, 30, ((int)windowBasePixels-pixelsWindowTop) );
+
+			//chão
+			g.setColor(Color.GREEN);
+			g.fillRect(1 ,380, 400, 20);
+			//bola
+			g.setColor(Color.red);
+			g.fillOval(197, (int)ballY, 7,	7);
+
+
+
+		}
+	}
+
 }
